18805d850442359b409db50b778fee56c15f2d9d
nicolas.andrade <nicolas2015fj@gmail.com>
2023-06-16 01:26:51 -0300

    ajustes finais jogo novo

--- src/Instruções/Creditos.java
+++ src/Instruções/Creditos.java
@@ -5,8 +5,20 @@ public class Creditos {
     private static class creditos{
         public void escritaCreditos(){
             System.out.println("_________ CRÉDITOS _________");
-            System.out.println("Programa escrito por: Nicolas Andrade ");
-            System.out.println("Data: -----------");
+            System.out.println(
+                    "Programa escrito por: Nicolas Andrade\n" +
+                    "Juan Marinho\n" +
+                    "Jaime Gadea\n" +
+                    "José Henrique Pereira"
+                    )
+            ;
+            System.out.println("Data de Finalização: 14/06/2023");
+            System.out.println(
+                    "Instituição: Centro Universitário Senac\n" +
+                    "Curso: TADS Turma A - Noite\n" +
+                    "Professor: Eduardo Takeo"
+                    )
+            ;
         }
     }
 
